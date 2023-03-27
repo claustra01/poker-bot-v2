767489d8af2811f1be3409a335799102f569154a
claustra01 <shinya.430.g@gmail.com>
2023-03-27 15:10:19 +0900

    :seedling: bot template

--- poker_bot_v2/main.py
+++ poker_bot_v2/main.py
@@ -1,12 +1,20 @@
 import os
 from dotenv import load_dotenv
+import discord
+from discord.ext import commands
 
 def main():
     
     load_dotenv(os.path.join(os.path.dirname(__file__), '../.env'))
     TOKEN = os.environ['DISCORD_BOT_TOKEN']
+
+    bot = commands.Bot(command_prefix='./', intents=discord.Intents.all())
+
+    @bot.command()
+    async def hello(ctx):
+        await ctx.send('Hello!')
     
-    print(TOKEN)
+    bot.run(TOKEN)
 
 if __name__ == '__main__':
     main()
