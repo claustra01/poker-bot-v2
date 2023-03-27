5b41f9c9166de0b65bcf3a110ba821ab3fb715bb
claustra01 <shinya.430.g@gmail.com>
2023-03-27 14:59:16 +0900

    :lock: get token

--- poker_bot_v2/main.py
+++ poker_bot_v2/main.py
@@ -1,5 +1,12 @@
+import os
+from dotenv import load_dotenv
+
 def main():
-    print('Hello!')
+    
+    load_dotenv(os.path.join(os.path.dirname(__file__), '../.env'))
+    TOKEN = os.environ['DISCORD_BOT_TOKEN']
+    
+    print(TOKEN)
 
 if __name__ == '__main__':
     main()
