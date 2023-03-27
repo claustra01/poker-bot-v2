4e84ca73d193b2907fe85a95331f0e7b9e6058f1
claustra01 <shinya.430.g@gmail.com>
2023-03-27 16:59:56 +0900

    :technologist: separate get_name() in Player

--- poker_bot_v2/player.py
+++ poker_bot_v2/player.py
@@ -1,18 +1,21 @@
 from medals import Medals
 
 class Player:
-    def __init__(self, id:str|None, key:str, rating:int, medals:Medals):
+    def __init__(self, id:str, key:str, rating:int, medals:Medals):
         self.id = id
         self.key = key
         self.rating = rating
         self.medals = medals
 
-    def get_name(self) -> str:
-        return self.id if (type(self.id) != None) else self.key
+    def get_id(self) -> str:
+        return self.id
+
+    def get_key(self) -> str:
+        return self.key
 
     def get_data(self) -> dict:
         obj:dict = {}
-        obj['id']:str = self.id if (type(self.id) != None) else None
+        obj['id']:str = self.id if (self.id == None) else None
         obj['key']:str = self.key
         obj['rating']:int = self.rating
         obj['medals']:Medals = self.medals.get_data()
