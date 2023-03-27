21810bbd581c8f86ba6b1b2233369248547e1fe4
claustra01 <shinya.430.g@gmail.com>
2023-03-27 18:36:33 +0900

    :sparkles: add id_to_key() in utils.py

--- poker_bot_v2/utils.py
+++ poker_bot_v2/utils.py
@@ -1,6 +1,12 @@
 from player import Player
 from medals import Medals
 
+def id_to_key(db:list[Player], id_or_key:str) -> str:
+    for i in range(len(db)):
+        if db[i].get_id() == id_or_key:
+            return db[i].get_key()
+    return id_or_key
+
 def key_to_player(db:list[Player], key:str) -> Player:
     for i in range(len(db)):
         if db[i].get_key() == key:
